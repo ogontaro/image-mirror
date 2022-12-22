b43fdabb6dd7506dbe0355afe612a27a3b5c4bb1
ogontaro <ogontaro@gmail.com>
2022-12-23 00:05:53 +0900

    refactor

--- sync.sh
+++ sync.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 IFS=$'\n\t'
-DEST_REPOSITORY="quay.io/image-mirror/"${SRC_REPOSITORY%/*}
 
+DEST_REPOSITORY="quay.io/image-mirror/"${SRC_REPOSITORY%/*}
 skopeo sync --all --preserve-digests --src docker --dest docker --authfile auth.json $SRC_REPOSITORY $DEST_REPOSITORY
